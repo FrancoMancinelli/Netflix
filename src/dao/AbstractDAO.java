7e7b33ed4dd4119cf27dc401681b5f8d9a385749
FrancoMancinelli <94619493+FrancoMancinelli@users.noreply.github.com>
2022-04-24 23:07:29 +0200

    Update AbstractDAO.java

--- src/dao/AbstractDAO.java
+++ src/dao/AbstractDAO.java
@@ -9,12 +9,8 @@ import java.sql.Statement;
 public abstract class AbstractDAO {
 	protected final String DB_URL = "jdbc:mysql://localhost/netflix";
 	protected final String USER = "root";
-<<<<<<< HEAD
 	protected final String PASS = "p4ssw0rd";
-=======
-	protected final String PASS = "root";
->>>>>>> 8cf94bff17e3b75cd3dc41c39f92dde1cc194fbf
-	
+
 	protected Connection conn;
 	protected Statement stmt;
 	
